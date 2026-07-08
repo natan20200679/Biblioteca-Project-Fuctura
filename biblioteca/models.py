eb02b7b212a8d6124f6e078a0278f01a7d5507c9
natan20200679 <natanaelmedrado@outlook.com>
2026-07-07 23:13:14 -0300

    Update columns of database and returns of data

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Livro(models.Model):
-    id = models.CharField()
+    id = models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=50)
     autor = models.CharField(max_length=25)
     descricao = models.CharField(max_length=100)
     ano = models.IntegerField(max_length=4)
     def __str__(self):
-        return self.titulo
+        return f'id(livro): {self.id}, título: {self.titulo}'
 
 class Usuario(models.Model):
     id = models.AutoField(primary_key=True)
@@ -15,15 +15,13 @@ class Usuario(models.Model):
     email = models.EmailField()
     telefone = models.CharField(max_length=10)
     def __str__(self):
-        return self.nome
+        return f'id(usuário): {self.id}, Nome: {self.nome}'
 
 class Emprestimo(models.Model):
-    def __init__(self, *args, **kwargs):
-        super().__init__(args, kwargs)
-        self.usuario = None
-    usuario_id = models.ForeignKey(Usuario.id, on_delete=models.CASCADE)
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     data_emprestimo = models.DateTimeField()
     data_devolucao = models.DateTimeField()
     def __str__(self):
-        return f'{self.usuario.nome} -> empréstimo em {self.data_emprestimo}'
+        return f'{self.usuario}\n{self.livro}\ndata(empréstimo): {self.data_emprestimo}'
 
