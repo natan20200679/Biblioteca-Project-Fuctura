af14d915c5b5c7c730f991785ebad6f35ee2827c
natan20200679 <natanaelmedrado@outlook.com>
2026-07-15 17:32:37 -0300

    Update "descadastrar" routes

--- biblioteca/urls.py
+++ biblioteca/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from biblioteca.views import (login_view, register_view, dashboard, visualizar_livros, cadastrar_livro, \
-                              visualizar_emprestimos, registrar_emprestimo, visualizar_usuarios, criar_usuario,
-                              visualizar_relatorios, excluir_usuario)
+from biblioteca.views import (login_view, register_view, dashboard, visualizar_livros,
+                              cadastrar_livro, descadastrar_livro, visualizar_emprestimos,
+                              registrar_emprestimo, visualizar_usuarios, criar_usuario,
+                              visualizar_relatorios, excluir_usuario, descadastrar_livros_lote)
 urlpatterns = [
     path('', login_view, name='login'),
     path('login/', login_view, name='login'),
@@ -10,7 +11,8 @@ urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
     path('livros/', visualizar_livros, name='livros'),
     path('livros/cadastrar/', cadastrar_livro, name='cadastrar_livro'),
-    # path('livros/descadastrar/<int:id>', descadastrar_livro, name='descadastrar_livro'),
+    path('livros/descadastrar/<int:id>', descadastrar_livro, name='descadastrar_livro'),
+    path("livros/descadastrar-lote/", descadastrar_livros_lote,name="descadastrar_livros_lote"),
     path('emprestimos/', visualizar_emprestimos, name='emprestimos'),
     path('emprestimos/registrar/', registrar_emprestimo, name='registrar_emprestimo'),
     path('usuarios/', visualizar_usuarios, name='usuarios'),
